4c6d87fcfa7dc229c06e409e9320cbf47cfa298c
Ask <ask@popgun.dk>
2021-09-23 20:28:18 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/general.css", function (req, res) {
+  res.sendFile(__dirname + "/" + "general.css");
+});
+
+io.on("connection", (socket) => {
+  console.log("connected");
+  socket.on("color-message", (msg) => {
+    // console.log("color-message", msg);
+    io.emit("color-message", msg);
+  });
+  socket.on("settings", (msg) => {
+    io.emit("settings", msg);
+  });
+});
+
+server.listen(5003, () => {
+  console.log("listening on *:5003");
+});
